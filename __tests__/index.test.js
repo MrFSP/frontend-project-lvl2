07d96c9861438aa4f9db37a8a9c5765ffc890c2f
Pavel Egorov <epe.m@mail.ru>
2021-11-21 00:42:56 +0300

    Add test for plane json files

---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { test, expect } from '@jest/globals';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log(`__dirname: ${__dirname}`);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const fixturePath = getFixturePath('expected.txt');
+const expected = fs.readFileSync(fixturePath, 'utf-8');
+
+test('json files difference', () => {
+  const before = getFixturePath('file1.json');
+  const after = getFixturePath('file2.json');
+  expect(genDiff(before, after)).toBe(expected);
+});
