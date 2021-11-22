60c788ac26b84d50649fb1021715cc828c164f50
Pavel Egorov <epe.m@mail.ru>
2021-11-23 01:20:06 +0300

    Add test for yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,17 +2,25 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
-import genDiff from '../index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const fixturePath = getFixturePath('expected.txt');
-const expected = fs.readFileSync(fixturePath, 'utf-8');
 
 test('json files difference', () => {
-  const before = getFixturePath('file1.json');
-  const after = getFixturePath('file2.json');
+  const fixturePath = getFixturePath('expected.json.txt');
+  const expected = fs.readFileSync(fixturePath, 'utf-8');
+  const before = getFixturePath('before.json');
+  const after = getFixturePath('after.json');
+  expect(genDiff(before, after)).toBe(expected);
+});
+
+test('yml files difference', () => {
+  const fixturePath = getFixturePath('expected.yaml.txt');
+  const expected = fs.readFileSync(fixturePath, 'utf-8');
+  const before = getFixturePath('before.yaml');
+  const after = getFixturePath('after.yaml');
   expect(genDiff(before, after)).toBe(expected);
 });
